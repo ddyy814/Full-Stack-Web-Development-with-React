2a36b809449b86e21e30c4392c4d1bbcec8db688
ddyy814 <dylanzhang0814@yahoo.com>
2021-01-20 12:10:18 -0800

    redux actions

--- React/confusion/src/components/DishdetailComponent.js
+++ React/confusion/src/components/DishdetailComponent.js
@@ -27,8 +27,7 @@ class CommentForm extends Component{
 
     handleSubmit(values){
         this.toggleModal();
-        console.log("Current State is: " + JSON.stringify(values));
-        alert("Current State is: " + JSON.stringify(values));
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render() {
@@ -92,7 +91,7 @@ class CommentForm extends Component{
         );
     }
 
-    function RenderComments({comments}) {
+    function RenderComments({comments, addComment, dishId}) {
         if(comments != null)        
             return (
                 <div className='col-12 col-md-5 m-1'>
@@ -114,7 +113,7 @@ class CommentForm extends Component{
                             );
                         })}
                     </ul>
-                    <CommentForm />
+                    <CommentForm dishId={dishId} addComment={addComment} />
                 </div>
             );
     }
@@ -141,7 +140,9 @@ class CommentForm extends Component{
                 </div>
                     <div className="row">
                         <RenderDish dish = {props.dish} />
-                        <RenderComments comments = {props.comments} />
+                        <RenderComments comments = {props.comments}
+                            addComment={props.addComment}
+                            dishId={props.dish.id} />
                     </div>
                     
                     
