ccb80a085b30246b834dddc24242f4fc0bb9c0ee
ddyy814 <dylanzhang0814@yahoo.com>
2021-01-22 12:28:37 -0800

    added Fetch Post Comment

--- React/confusion/src/components/DishdetailComponent.js
+++ React/confusion/src/components/DishdetailComponent.js
@@ -29,7 +29,7 @@ class CommentForm extends Component{
 
     handleSubmit(values){
         this.toggleModal();
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render() {
@@ -93,7 +93,7 @@ class CommentForm extends Component{
         );
     }
 
-    function RenderComments({comments, addComment, dishId}) {
+    function RenderComments({comments, postComment, dishId}) {
         if(comments != null)        
             return (
                 <div className='col-12 col-md-5 m-1'>
@@ -103,19 +103,12 @@ class CommentForm extends Component{
                             return (
                                 <li key={comment.id}>
                                     <p>{comment.comment}</p>
-                                    <p>-- {comment.author},
-                                    &nbsp;
-                                    {new Intl.DateTimeFormat('en-US', {
-                                        year: 'numeric',
-                                        month: 'long',
-                                        day: '2-digit'
-                                        }).format(new Date(comment.date))}
-                                    </p>
+                                    <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US' ,{ year:'numeric',month:'long',day:'2-digit',}).format(new Date(comment.date))}</p>
                                 </li>
                             );
                         })}
                     </ul>
-                    <CommentForm dishId={dishId} addComment={addComment} />
+                    <CommentForm dishId={dishId} postComment={postComment} />
                 </div>
             );
     }
@@ -161,7 +154,7 @@ class CommentForm extends Component{
                     <div className="row">
                         <RenderDish dish = {props.dish} />
                         <RenderComments comments = {props.comments}
-                            addComment={props.addComment}
+                            postComment={props.postComment}
                             dishId={props.dish.id} />
                     </div>
                     
